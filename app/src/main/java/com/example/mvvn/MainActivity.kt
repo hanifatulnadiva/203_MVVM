52385788dcd2934405e15b559fa518c6dd574530
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 17:16:40 +0700

    menmanggil function navigasii untuk menjalankan project

--- app/src/main/java/com/example/mvvn/MainActivity.kt
+++ app/src/main/java/com/example/mvvn/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mvvn.ui.theme.MVVNTheme
+import com.example.mvvn.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MVVNTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
