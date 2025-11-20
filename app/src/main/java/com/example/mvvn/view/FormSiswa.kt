2d64d9500a45cf37296a198fbe3cdfa89a52142a
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 09:35:01 +0700

    add fun FormSiswa yang memiliki parameter pilihanJK, onSubmitButtonClick dan modifier

---
+++ app/src/main/java/com/example/mvvn/view/FormSiswa.kt
@@ -0,0 +1,15 @@
+package com.example.mvvn.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClick:(MutableList<String>)->Unit,
+    modifier: Modifier=Modifier
+    ){
+
+}
