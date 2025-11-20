1efb1e2148ecd6aa633d25a483b0eef314bf4447
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 09:25:44 +0700

    add  variabel statusUI yang terikat dengan variabel private _statusUI jadi ini untuk dta bindingnya saling terkait

--- app/src/main/java/com/example/mvvn/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/mvvn/viewmodel/SiswaViewModel.kt
@@ -9,6 +9,8 @@ import kotlinx.coroutines.flow.update
 
 class SiswaViewModel : ViewModel() {
     private val _statusUI = MutableStateFlow(value = Siswa())
+    val statusUI: StateFlow <Siswa> =_statusUI.asStateFlow()
+
 
 
 }
