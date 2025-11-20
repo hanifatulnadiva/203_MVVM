514afc831795ad8ce9e6e64c87ab1deb975dccaf
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 16:17:06 +0700

    Add function SiswaApp dengan parameter modifier, view model dan navController

--- app/src/main/java/com/example/mvvn/view/uicontroller/navigasi.kt
+++ app/src/main/java/com/example/mvvn/view/uicontroller/navigasi.kt
@@ -1,6 +1,23 @@
 package com.example.mvvn.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.mvvn.viewmodel.SiswaViewModel
+
 enum class Navigasi{
     Formulir,
     Detail
+}
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController= rememberNavController()
+){
+
 }
