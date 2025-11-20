0818eecd07ba33b9873d99622c88c842e5a5e2d2
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 15:30:14 +0700

    add fun TampilSiswa yang mempunyai parameter statusUISiswa :Siswa dan onBackButtonClick:()-> Unit

---
+++ app/src/main/java/com/example/mvvn/view/TampilSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.mvvn.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import com.example.mvvn.model.Siswa
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    statusUISiswa :Siswa,
+    onBackButtonClick:()-> Unit
+){
+
+}
