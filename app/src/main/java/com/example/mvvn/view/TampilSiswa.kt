5017241a19f7f068c4a1d49662473a81264e5c02
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 15:46:19 +0700

    Add variabel tetap nama variabelnya itu items, nah disini terdapat 2 nilai yaitu untuk menyimpan judulnya (Nama Lengkap, Alamat, Jenis Kelamin) dengan menambahkan resource nya untuk yang kedua itu berisi nilai inputan nama, gender sama alamat ini diambil dari class Siswa

--- app/src/main/java/com/example/mvvn/view/TampilSiswa.kt
+++ app/src/main/java/com/example/mvvn/view/TampilSiswa.kt
@@ -2,7 +2,9 @@ package com.example.mvvn.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import com.example.mvvn.model.Siswa
+import com.example.mvvn.R
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
@@ -10,5 +12,10 @@ fun TampilSiswa(
     statusUISiswa :Siswa,
     onBackButtonClick:()-> Unit
 ){
+    val items = listOf(
+        Pair (first = stringResource(R.string.nama), second = statusUISiswa.nama),
+        Pair (first = stringResource(R.string.gender), second = statusUISiswa.gender),
+        Pair (first = stringResource(R.string.alamat), second = statusUISiswa.alamat),
 
+    )
 }
