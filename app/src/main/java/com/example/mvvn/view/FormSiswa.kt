c5912c01efd5465a713b2010fe03ed23f4797878
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 09:45:33 +0700

    add Top app bar di dalam scaffold terus ngasi title untuk halaman FormSiswa dengan warna text nya itu putih terus yang text itu mangggil resource yang sudah di buat tadi

--- app/src/main/java/com/example/mvvn/view/FormSiswa.kt
+++ app/src/main/java/com/example/mvvn/view/FormSiswa.kt
@@ -1,12 +1,18 @@
 package com.example.mvvn.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.example.mvvn.R
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
@@ -19,4 +25,7 @@ fun FormSiswa(
     var textAlamat by remember { mutableStateOf(value = "") }
     var textGender by remember { mutableStateOf(value = "") }
     val listData: MutableList<String> = mutableListOf(textNama, textAlamat, textGender)
+    Scaffold (modifier= Modifier,
+        topBar = { TopAppBar(title = { Text(stringResource(R.string.app_name), color= Color.White)})
+        }){}
 }
